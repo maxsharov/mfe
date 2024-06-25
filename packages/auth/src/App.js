6968b47fe97a5f648c0a330c8f88cc7c10d0695d
Maksym Sharov <max@Maksyms-Laptop.local>
2024-06-25 15:50:52 -0600

    update import path

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Router } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import SignIn from './components/Signin'
-import SignUp from './components/SignUp'
+import SignUp from './components/Signup'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au'
